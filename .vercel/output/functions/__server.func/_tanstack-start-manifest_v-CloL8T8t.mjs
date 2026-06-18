4584a09a21a9e45cf22fa549e89dff50e441367a
Nathan Pinaffi <pinaffin905@gmail.com>
2026-06-17 22:49:21 -0300

    Updates

---
+++ .vercel/output/functions/__server.func/_tanstack-start-manifest_v-CloL8T8t.mjs
@@ -0,0 +1,27 @@
+//#region node_modules/.nitro/vite/services/ssr/assets/_tanstack-start-manifest_v-CloL8T8t.js
+var tsrStartManifest = () => ({
+	routes: {
+		__root__: {
+			filePath: "C:/Users/pinaf/codes/portifolio/src/routes/__root.tsx",
+			children: ["/"],
+			assets: [{
+				tag: "link",
+				attrs: {
+					rel: "stylesheet",
+					href: "/assets/index-C9tdu09w.css",
+					type: "text/css"
+				}
+			}],
+			preloads: ["/assets/index-DNNnafIS.js"]
+		},
+		"/": {
+			filePath: "C:/Users/pinaf/codes/portifolio/src/routes/index.tsx",
+			children: void 0,
+			assets: [],
+			preloads: ["/assets/routes-Dp5qrGfV.js"]
+		}
+	},
+	clientEntry: "/assets/index-DNNnafIS.js"
+});
+//#endregion
+export { tsrStartManifest };
